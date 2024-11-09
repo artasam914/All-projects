80ae5f63f19ca7065a952f657255aab36ef087ff
Artasam javed <artasam914@gmail.com>
2024-11-09 04:53:26 -1200

    Counter
    count the total and remaining words.

---
+++ counter input/script.js
@@ -0,0 +1,13 @@
+const remaining=document.querySelector("#remainingnumber")
+const textarea=document.querySelector("#textarea")
+const total=document.querySelector("#totalnum")
+
+
+textarea.addEventListener("keyup",()=>{
+    updateCounter()
+})
+
+function updateCounter(){
+   total.innerText=textarea.value.length
+remaining.innerText=textarea.getAttribute("maxLength") - textarea.value.length
+}
