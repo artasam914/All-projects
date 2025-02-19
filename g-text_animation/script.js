fd77d2b17fca424f1e98da9b7604ab5fc468aa0e
Artasam javed <artasam914@gmail.com>
2025-02-19 22:06:10 +0500

    script.js

--- g-text_animation/script.js
+++ g-text_animation/script.js
@@ -1,33 +1,5 @@
-// function textanimation() {
-//   // Select the heading element from html
-//  let head1 = document.querySelector(".heading");
-//  // Get the text content of the heading element
-//  let h1Text = head1.textContent;
 
-//  // Split the text content into an array of characters
-//  let splitedText = h1Text.split("");
-//  // console.log(splitedText)
 
-//  // Create a variable to store the cluttered text
-//  let clutter = "";
-//  // Loop through the array of characters and create a span element for each character
-//  splitedText.forEach((elem) => {
-//    clutter += `<span>${elem}</span>`;
-//  });
-//  // console.log(clutter)
-//  // Set the innerHTML of the heading element to the cluttered text
-//  head1.innerHTML = clutter;
-//  // console.log(h1.innerHTML)
-
-//  // Use GSAP to animate the span elements
-//  gsap.from("span", {
-//    //it span will not work so make them inline block  .//can also target span alone ..
-//    y: 100,
-//    duration: 1,
-//    stagger: 0.2,
-//  });
-// }
-// textanimation();
 
 function textAnimationSidesToCenter() {
   let h1 = document.querySelector(".heading");
