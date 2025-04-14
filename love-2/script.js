eedc896934a14c4b42c7364ba4f799b501cc6fab
Artasam javed <artasam914@gmail.com>
2025-04-14 23:59:17 +0500

    Update script.js

--- love-2/script.js
+++ love-2/script.js
@@ -7,7 +7,7 @@ image.addEventListener("dblclick", () => {
 
 // icon ko style diya he 
     icon.style.transform = 'translate(-50%,-50%) scale(1.3) '
-    
+     
 icon.style.opacity=0.7
 
     setTimeout(()=>{
